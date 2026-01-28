108f0e887af790b93af6249ebc963fd1e7e00e3d
Artem Artemenko <temamet@gmail.com>
2026-01-28 15:41:38 +0200

    Update game

--- sw.js
+++ sw.js
@@ -13,7 +13,15 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
